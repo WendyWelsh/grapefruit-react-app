9644a1f45d727fed3b62e81b64eb7d46579add6b
Wendy Welsh <wwccny@hotmail.com>
2019-02-14 20:47:36 -0700

    login

---
+++ src/Register.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+
+
+class Register extends React.Component {
+  state = {
+    username: '',
+    password: '',
+    email: '',
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="standard-name"
+          label="Username"
+          className={classes.textField}
+          value={this.state.name}
+          onChange={this.handleChange('name')}
+          margin="normal"
+        />
+
+        <TextField
+          id="standard-password-input"
+          label="Password"
+          className={classes.textField}
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+        />
+
+        <TextField
+          id="standard-textarea"
+          label="Email"
+          placeholder="Email"
+          multiline
+          className={classes.textField}
+          margin="normal"
+        />
+        </form>
+)}}
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Register);
