c24c194f47c8f7f222fc74330b897c34d8014096
Matt Schmitt <mw.schmitt2@gmail.com>
2019-02-15 20:35:01 -0700

    added WorkoutForm and Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AccountForm from './AccountForm';
+//import AccountForm from './AccountForm';
+import WorkoutForm from './WorkoutForm';
+//import MacroForm from './MacroForm';
 class App extends Component {
   render() {
     return (
    <div>
-     <AccountForm />
+     <WorkoutForm />
    </div>
     );
   }
