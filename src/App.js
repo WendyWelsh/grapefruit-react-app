31071433e3a68a48f2156edb507868d0e5ff1692
Wendy Welsh <wwccny@hotmail.com>
2019-02-19 13:41:31 -0700

    clientlist, Coachlist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import AccountForm from './AccountForm';
+// import AccountForm from './AccountForm';
+// import CoachList from './CoachList';
+import ClientList from './ClientList';
+// import Login from './Login';
+
+
 class App extends Component {
   render() {
     return (
-   <div>
-     <AccountForm />
-   </div>
+      <div>
+        {/* <AccountForm /> */}
+        {/* <Login /> */}
+        {/* <CoachList /> */}
+        <ClientList />
+
+       
+      // </div>
     );
   }
 }
+
 
 export default App;
