c00d5336847aeb69ab49c6c39ff2cbc5bf6bdbce
Angela Montanye <Angela.montanye@gmail.com>
2019-02-23 10:35:19 -0700

    checking development situation

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
               <Route path="/clientlist" component={ClientList} />
               <Route path="/coachlist" component={CoachList} />
               <Route path="/workoutform" component={WorkoutForm} />
+              
             </div>
           </MuiThemeProvider>
         </div>
