04102af954bc30c4defffe3eb8dd51521ac19bc6
Wendy Welsh <wwccny@hotmail.com>
2019-03-22 10:07:31 -0600

    logout button works

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,6 +9,8 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import NavBarButton from './NavBarButton';
 import { InputLabel } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+
 
 const styles = {
   root: {
@@ -23,35 +25,61 @@ const styles = {
   },
 };
 
-function NavBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar className="logo"  position="static">
-        <Toolbar  >
-          {/* <IconButton className="{classes.menuButton}" color="inherit" aria-label="Menu">       
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      anchorEl: null,
+
+    };
+    this.directToLogin = this.directToLogin.bind(this)
+  }
+  
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  directToLogin() {
+    this.props.history.push("/");
+    console.log("pushed")
+  };
+  render() {
+
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar className="logo" position="static">
+          <Toolbar  >
+            {/* <IconButton className="{classes.menuButton}" color="inherit" aria-label="Menu">       
          <NavBarButton/>
           </IconButton> */}
-          {/* <div className="icon" ></div> */}
-          <Typography  className={classes.grow}>
-          
-            {/* Grapefruit */}
-            {/* <GrapefruitLogo /> */}
-            {/* <svgIcon width="568" height="150" viewBox="0 0 568 150" fill="none" xmlns="http://www.w3.org/2000/svg"
+            {/* <div className="icon" ></div> */}
+            <Typography className={classes.grow}>
+
+              {/* Grapefruit */}
+              {/* <GrapefruitLogo /> */}
+              {/* <svgIcon width="568" height="150" viewBox="0 0 568 150" fill="none" xmlns="http://www.w3.org/2000/svg"
             ></svgIcon> */}
-           
-          {/* <img src='images/grapfruitlogo.jpg' variant="h6" color="inherit" width="80" height="50"/> */}
-          
-          </Typography>
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+
+              {/* <img src='images/grapfruitlogo.jpg' variant="h6" color="inherit" width="80" height="50"/> */}
+
+            </Typography>
+            <Button
+              onClick={this.directToLogin}
+              color="inherit">Logout</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 NavBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(NavBar);
+export default withRouter(withStyles(styles)(NavBar));
